2d538819a52a02bdabdaab35e7dad795475d5817
WindowsM16a <wuvershayne@gmail.com>
2024-09-24 16:21:02 +0000

    fns

---
+++ tuts/7.js
@@ -0,0 +1,76 @@
+// function greet(name) {
+//     if (!name) {
+//         console.log("Hi there!")
+//     } else {
+//         console.log(`Hello ${name}!`)
+//     }
+// }
+
+// greet("Shayne");
+// greet();
+// greet("John");
+// greet();
+
+
+// function convertToFahrenheit(celsius) {
+//     tempInFahr = (celsius * 9 / 5) + 32;
+//     return tempInFahr;
+// }
+// console.log(convertToFahrenheit(25));
+
+// function convertToCelsius(fahrenheit) {
+//     tempInCelsius = (fahrenheit - 32) * 5 / 9;
+//     return tempInCelsius;
+// }
+// console.log(convertToCelsius(86));
+
+
+// function convertTemperature(degrees, unit) {
+//     function convertToCelsius(degrees) {
+//         let tempInCelsius = (degrees - 32) * 5 / 9;
+//         return tempInCelsius;
+//     }
+//     function convertToFahrenheit(degrees) {
+//         let tempInFahr = (degrees * 9 / 5) + 32;
+//         return tempInFahr;
+//     }
+//     if (unit === 'C') {
+//         return convertToCelsius(degrees);
+//     } else if (unit === 'F') {
+//         return convertToFahrenheit(degrees);
+//     } else {
+//         throw new Error("nigga")
+//     }
+// }
+
+// console.log(convertTemperature(86, 'x'))
+// console.log(convertTemperature(25, 'F'))
+
+function convertLength(length, from, to) {
+    // 1 mile = 1.6km
+    // 1 mile = 5280ft
+    // 1km = 3281ft
+    function convertToMiles(length) {
+        let lengthInMiles = length * 1.6;
+        return lengthInMiles;
+    }
+    function convertToKm(length) {
+        let lengthInKm = length / 1.6; 
+        return lengthInKm;
+    }
+    if (from === 'miles' && to === 'km') {
+        return convertToKm(length);
+    } else if (from === 'km' && to === 'miles') {
+        return convertToMiles(length);
+    } else {
+        console.log(`Invalid unit: ${unit}`)
+    }
+}
+
+console.log(convertLength(50, 'miles', 'km'))
+console.log(convertLength(32, 'km', 'miles'))
+console.log(convertLength(50, 'miles', 'km'))
+console.log("")
+console.log(convertLength(5, 'miles', 'km'))
+console.log(convertLength(5, 'miles', 'ft'))
+console.log(convertLength(5, 'km', 'ft'))
