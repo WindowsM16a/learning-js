a0c435429b5349619808d777dbb24eae850c1056
WindowsM16a <wuvershayne@gmail.com>
2024-09-25 22:32:24 +0000

    7 complete

--- tuts/cart_qty.js
+++ tuts/cart_qty.js
@@ -1,5 +1,38 @@
 var cart_qty = 0;
 
+function updateCartQuantity(change) {
+    if (change > 0 && change + cart_qty > 10) {
+        alert("The cart is full")
+        return;
+    } 
+    if (change < 0 && change + cart_qty < 0) {
+        alert("The cart is empty")
+        return;
+    }
+
+    cart_qty = cart_qty + change;
+
+    if (change === 0) {
+        console.log("Cart was cleared")
+        return;
+    }
+
+    console.log(`Cart quantity: ${cart_qty}`)
+    
+}
+
+
+
+// function updateCartQuantity(change) {
+//     if (cart_qty <= change){
+//         alert('Not enough items in the cart')
+//         console.log(`Cart quantity: ${cart_qty}`);
+//       } else{
+//         cart_qty -= 2;
+//         console.log(`Cart quantity: ${cart_qty}`);
+//       }
+// }
+
 
 // var fahrenheit;
 // var celsius;
