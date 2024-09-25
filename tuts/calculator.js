ad6a7ea28ca126a9e4737a6e03382afcf621b1fc
WindowsM16a <wuvershayne@gmail.com>
2024-09-25 21:00:37 +0000

    fn for calc

--- tuts/calculator.js
+++ tuts/calculator.js
@@ -1,2 +1,11 @@
 let calculation = '';
 console.log(calculation)
+
+function calcLogic(value) {
+    if (value == '=') {
+        calculation = eval(calculation);
+    } else {
+        calculation += value;
+    }
+    console.log(calculation)
+}
