c0042a38d7335d20c17c8b23ae11cdcc4d921c6e
WindowsM16a <wuvershayne@gmail.com>
2024-09-29 15:14:48 +0000

    marina timeee

---
+++ tuts/9ag.js
@@ -0,0 +1,31 @@
+
+
+
+
+function display9a() {
+    console.log(document.querySelector("#nAButton").innerHTML);
+}
+
+function change9b() {
+    let newTxt = document.querySelector("#nBButton").innerHTML = "9b done!";
+    console.log(newTxt)
+}
+
+function displayChoice(choice) {
+    if (choice === "heads") {
+        document.querySelector("#displayParagraph").innerHTML = "heads";
+    } else {
+        document.querySelector("#displayParagraph").innerHTML = "tails";
+    }
+    console.log(document.querySelector("#displayParagraph").innerHTML);
+}
+
+
+
+document.getElementById("nAButton").addEventListener("click", () => display9a());
+
+document.getElementById("nBButton").addEventListener("click", () => change9b());
+
+document.getElementById("headsButton").addEventListener("click", () => displayChoice("heads"));
+
+document.getElementById("tailsButton").addEventListener("click", () => displayChoice("tails"));
